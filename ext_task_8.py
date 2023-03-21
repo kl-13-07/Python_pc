71044a302469a82778c5ff4eec69b7c637080814
KL <oleg.kostyukof@yandex.ru>
2023-03-21 18:16:40 +0300

    work on mistakes3

--- ext_task_8.py
+++ ext_task_8.py
@@ -8,7 +8,7 @@
 n = int(input('Введите  n'))
 m = int(input('Введите  m'))
 k = int(input('Введите  k'))
-if k < n * m and ((k % n == 0) or (k % m == 0)):
+if k < n * m and k % n == 0 or k % m == 0:
     print('yes')
 else:
     print('no')
